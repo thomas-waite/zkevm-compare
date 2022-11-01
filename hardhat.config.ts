8f3da766e38faa6f274b27147776b87c21a3bf65
thomas-waite <thomas.waite4@gmail.com>
2022-10-31 20:10:58 -0700

    feat: setup zksync

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
 
 dotenv.config();
 
@@ -15,11 +17,31 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  zksolc: {
+    version: "1.2.0",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+      experimental: {
+        dockerImage: "matterlabs/zksolc",
+        tag: "v1.2.0",
+      },
+    },
+  },
+  zkSyncDeploy: {
+    zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
+    ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+  },
   networks: {
     scrollTestnet: {
       url: process.env.SCROLL_TESTNET_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    hardhat: {
+      zksync: true,
     },
   },
 };
